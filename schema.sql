8c5142ebd7bf0839795a7fd3c937fdc91f526e9c
ValeoPrime <primarh111@mail.ru>
2019-08-19 20:34:26 +0300

    1

--- schema.sql
+++ schema.sql
@@ -9,18 +9,18 @@ CREATE TABLE projects (
 
 CREATE TABLE tasks (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date_of_creation DATETIME(6),
+  date_of_creation DATE(10),
   status INT(1),
   task_title VARCHAR(80),
   task_file VARCHAR(30),
-  deadline DATETIME(6),
+  deadline DATE(10),
   user_id INT(10),
   project_id INT(10)
 );
 
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  date_of_registration DATETIME(6),
+  date_of_registration DATE(10),
   email VARCHAR(40),
   user_name VARCHAR(40),
   password VARCHAR(30)
