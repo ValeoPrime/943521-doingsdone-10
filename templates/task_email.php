71bdf21ef75879bed8c3c7b7b4c992c9cb3888c5
ValeoPrime <primarh111@mail.ru>
2019-09-22 11:46:07 +0300

    1

---
+++ templates/task_email.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+<h1>Уведомление от сервиса «Дела в порядке»</h1>
+
+<p>Уважаемый, <?=$users['user_name']; ?>. У вас запланирована задача  <?=$text; ?> на <?=$tasks['deadline']; ?></p>
+
+
+</body>
+</html>
